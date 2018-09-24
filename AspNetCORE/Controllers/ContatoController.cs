c882745cdbbe159c55000c29599da274b5fdbab4
Focus Sistemas <pdvfocus@gmail.com>
2018-09-24 10:44:34 -0300

    Adicionar arquivos de projeto.

---
+++ AspNetCORE/Controllers/ContatoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCORE.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCORE.Controllers
+{
+    public class ContatoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReceberContato([FromForm]Contato contato)
+        {
+
+            string conteudo = String.Format("Nome: {0}, E-mail: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+
+            return new ContentResult() { Content = conteudo };
+
+        }
+
+        /*
+        public IActionResult ReceberContato()
+        {
+            Contato contato = new Contato();
+
+            contato.Nome = Request.Form["nome"];
+            contato.Email = Request.Form["email"];
+            contato.Assunto = Request.Form["assunto"];
+            contato.Mensagem = Request.Form["mensagem"];
+
+            string conteudo = String.Format("Nome: {0}, E-mail: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+
+            return new ContentResult() { Content = conteudo };
+        }
+        */
+
+    }
+}
